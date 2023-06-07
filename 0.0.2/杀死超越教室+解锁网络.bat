200370fa888be3c01150e256ad7accd4be375f1a
HuChai <Hu39066123@outlook.com>
2023-06-07 18:06:32 +0800

    2023-6-7

---
+++ 0.0.2/杀死超越教室+解锁网络.bat
@@ -0,0 +1,5 @@
+del /f StudentMain.exe
+del /f GATESRV.exe
+del /f LibElcLogU.dll
+del /f taskkill /f /im log4cplusU.dll
+copy hosts C:\windows\system32\drivers\etc\hosts
---
+++ 0.0.2/脱离控制+解锁网络.bat
@@ -0,0 +1,5 @@
+taskkill /f /im StudentMain.exe
+taskkill /f /im GATESRV.exe
+taskkill /f /im LibElcLogU.dll
+taskkill /f /im log4cplusU.dll
+copy hosts C:\windows\system32\drivers\etc\hosts
